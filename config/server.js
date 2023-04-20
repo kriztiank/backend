217291f8117ef35f5101c9c6b4c73d58d406a03f
kriztiank <kriztiank@gmail.com>
2023-04-20 08:44:01 +0200

    dj-events-backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1d998ee222e6aaa0a7a86251351df618'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28143554-571a-4937-ab91-d8cd21c42bfb'
+};
